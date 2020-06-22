981f6a6bdbac3f2435dcd559e426de8552815af5
Simon Jang <simon.jang.contact@gmail.com>
2020-06-22 19:52:40 +0200

    Add stream-to-observable and remove custom stream implementation

--- source/index.ts
+++ source/index.ts
@@ -2,39 +2,12 @@ import { ReadStream } from 'fs';
 import { Observable } from 'rxjs';
 import * as split from 'split';
 import * as stripBOM from 'strip-bom-stream';
-import { JsonObject } from 'type-fest';
-
-/**
- * Create an observable steam based from stream events
- *
- * @param stream - Stream to create the events from.
- */
-export const streamToObservable = <T>(stream: ReadStream): Observable<T> => {
-	return new Observable((observer): any => {
-		stream
-			.on('data', async (chunk: T) => {
-				observer.next(chunk);
-			})
-			.on('error', err => {
-				observer.error(err);
-			})
-			.on('end', () => {
-				observer.complete();
-			});
-
-		// tslint:disable-next-line:typedef
-		return () => {
-			stream.destroy();
-		};
-	});
-};
-
+import * as streamToObservable from '@samverschueren/stream-to-observable';
 
 /**
  * Converts the NDJSON stream to an observable that will emit every record
  *
  * @param stream - NDJSON read stream
  */
-export const ndjsonToObservable = <T  = unknown>(stream: ReadStream): Observable<T> => {
-	return streamToObservable(stream.pipe(stripBOM()).pipe(split(/(?<=})\n(?={)/, JSON.parse)))
-}
+export const ndjsonToObservable = <T = unknown>(stream: ReadStream): Observable<T> =>
+	streamToObservable(stream.pipe(stripBOM()).pipe(split(/(?<=})\n(?={)/, JSON.parse)));
